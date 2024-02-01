b7b7e3c9aa397731a9cfc2ce5e9d2292f3d9a869
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-02-01 19:01:08 -0300

    findAll created in Team Service

--- src/main/java/com/minasfut/minasfut/services/TeamService.java
+++ src/main/java/com/minasfut/minasfut/services/TeamService.java
@@ -1,4 +1,20 @@
 package com.minasfut.minasfut.services;
 
+import com.minasfut.minasfut.dto.TeamDTO;
+import com.minasfut.minasfut.entities.Team;
+import com.minasfut.minasfut.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TeamService {
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public List<TeamDTO> findAll(){
+        List<Team> result = teamRepository.findAll();
+        return result.stream().map(TeamDTO::new).toList();
+    }
 }
