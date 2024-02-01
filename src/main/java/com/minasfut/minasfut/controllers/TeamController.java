fd275dbe16aa908ad2444891a563f3488372f43e
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-02-01 19:05:26 -0300

    Method findall created on Team Controller

--- src/main/java/com/minasfut/minasfut/controllers/TeamController.java
+++ src/main/java/com/minasfut/minasfut/controllers/TeamController.java
@@ -1,4 +1,21 @@
 package com.minasfut.minasfut.controllers;
 
+import com.minasfut.minasfut.dto.TeamDTO;
+import com.minasfut.minasfut.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/team")
 public class TeamController {
+    @Autowired
+    private TeamService teamService;
+
+    public List<TeamDTO> findAll(){
+        List<TeamDTO> result = teamService.findAll();
+        return result;
+    }
 }
